de8253c6c5cd81e3b7a2dff422ccb8982d73ca2c
Eric Backhage <eric.backhage@gmail.com>
2018-06-06 20:57:28 +0200

    Created ASP.NET Core MVC project and added a TO-DO list

---
+++ LittleAspNetCoreBook/AspNetCoreTodo/AspNetCoreTodo/Services/ITodoItemService.cs
@@ -0,0 +1,10 @@
+using AspNetCoreTodo.Models;
+using System.Threading.Tasks;
+
+namespace AspNetCoreTodo.Services
+{
+    public interface ITodoItemService
+    {
+        Task<TodoItem[]> GetIncompleteItemsAsynch();
+    }
+}
