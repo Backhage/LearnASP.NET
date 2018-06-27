f8641a605119b09236e907ee7c7eca2b44d9d620
Eric Backhage <eric.backhage@gmail.com>
2018-06-27 23:11:12 +0200

    Added test project

---
+++ WorkingWithVisualStudio/WorkingWithVisualStudio.Tests/ProductTests.cs
@@ -0,0 +1,24 @@
+﻿using WorkingWithVisualStudio.Models;
+using Xunit;
+
+namespace WorkingWithVisualStudio.Tests
+{
+    public class ProductTests
+    {
+        [Fact]
+        public void CanChangeProductName()
+        {
+            var p = new Product { Name = "Test", Price = 100m };
+            p.Name = "New Name";
+            Assert.Equal("New Name", p.Name); 
+        }
+
+        [Fact]
+        public void CanChangeProductPrice()
+        {
+            var p = new Product { Name = "Test", Price = 100m };
+            p.Price = 200m;
+            Assert.Equal(200m, p.Price);
+        }
+    }
+}
