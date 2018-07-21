5245cd870fd35e51c1ae33fc166d84c6d0704a13
Eric Backhage <eric.backhage@gmail.com>
2018-07-21 23:58:08 +0200

    Added Cart class

---
+++ SportStore/SportStore/Models/Cart.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SportStore.Models
+{
+    public class Cart
+    {
+        private readonly List<CartLine> lineCollection = new List<CartLine>();
+
+        public virtual void AddItem(Product product, int quantity)
+        {
+            var line = lineCollection.FirstOrDefault(p => p.Product.ProductID == product.ProductID);
+            if (line == null)
+            {
+                lineCollection.Add(new CartLine { Product = product, Quantity = quantity });
+            }
+            else
+            {
+                line.Quantity += quantity;
+            }
+        }
+
+        public virtual void RemoveLine(Product product) => lineCollection.RemoveAll(p => p.Product.ProductID == product.ProductID);
+
+        public virtual decimal ComputeTotalValue() => lineCollection.Sum(e => e.Product.Price * e.Quantity);
+
+        public virtual void Clear() => lineCollection.Clear();
+
+        public IEnumerable<CartLine> Lines => lineCollection;
+    }
+
+    public class CartLine
+    {
+        public int CartLineID { get; set; }
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
