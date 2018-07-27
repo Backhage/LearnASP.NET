b3b60261502986fe566eead2ef89887c213d3bde
Eric Backhage <eric.backhage@gmail.com>
2018-07-27 23:33:11 +0200

    Added checkout form

---
+++ SportStore/SportStore/Controllers/OrderController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportStore.Models;
+
+namespace SportStore.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult Checkout()
+        {
+            return View(new Order());
+        }
+    }
+}
