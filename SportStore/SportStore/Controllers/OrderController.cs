15fe1983494cccdafb748561f0b8df8ee632fd83
Eric Backhage <eric.backhage@gmail.com>
2018-07-30 00:36:03 +0200

    Finished checkout procedure

--- SportStore/SportStore/Controllers/OrderController.cs
+++ SportStore/SportStore/Controllers/OrderController.cs
@@ -1,13 +1,48 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SportStore.Models;
+using System.Linq;
 
 namespace SportStore.Controllers
 {
     public class OrderController : Controller
     {
+        private readonly IOrderRepository _repository;
+        private readonly Cart _cart;
+
+        public OrderController(IOrderRepository repository, Cart cart)
+        {
+            _repository = repository;
+            _cart = cart;
+        }
+
         public IActionResult Checkout()
         {
             return View(new Order());
+        }
+
+        [HttpPost]
+        public IActionResult Checkout(Order order)
+        {
+            if (_cart.Lines.Count() == 0)
+            {
+                ModelState.AddModelError("", "Sorry, your cart is empty!");
+            }
+            if (ModelState.IsValid)
+            {
+                order.Lines = _cart.Lines.ToArray();
+                _repository.SaveOrder(order);
+                return RedirectToAction(nameof(Completed));
+            }
+            else
+            {
+                return View(order);
+            }
+        }
+
+        public IActionResult Completed()
+        {
+            _cart.Clear();
+            return View();
         }
     }
 }
--- SportStore/SportStore/Startup.cs
+++ SportStore/SportStore/Startup.cs
@@ -24,6 +24,7 @@ namespace SportStore
             services.AddTransient<IProductRepository, EFProductRepository>();
             services.AddScoped(sp => SessionCart.GetCart(sp));
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddTransient<IOrderRepository, EFOrderRepository>();
             services.AddMvc();
             services.AddMemoryCache();
             services.AddSession();
